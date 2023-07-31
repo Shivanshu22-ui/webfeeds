b6099b58a0d64028fbcaeb59a2962319c8b145d0
= <=>
2023-07-31 22:52:58 +0530

    env setup

--- src/apis/instance.js
+++ src/apis/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const NEXT_APP_API_BASE_URL = "https://api.unsplash.com";
-const NEXT_APP_API_KEY = "1L47xI7TuQHBeiQhOrEETmfpb28a1-9FdJeoOw2-ufs";
+const NEXT_APP_API_KEY =process.env.NEXT_PUBLIC_KEY;
 
 const openInstance = axios.create({
     baseURL: NEXT_APP_API_BASE_URL,
